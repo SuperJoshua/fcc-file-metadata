df2a6632db53325d13dd4c310f8630f1129033c3
Joshua Wright <1981283+SuperJoshua@users.noreply.github.com>
2025-05-11 00:33:06 +0000

    passed FCC tests

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+"use strict"
 
-var app = express();
+const cors = require("cors")
+const express = require("express")
+const multer = require("multer")
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+const app = express()
+const upload = multer()
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+app.use(cors())
+app.use("/public", express.static(`${process.cwd()}/public`))
 
+app.get("/", function (req, res) {
+   res.sendFile(`${process.cwd()}/views/index.html`)
+})
 
+// solution
 
+// Wow, this one was stupid-easy.
 
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-});
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+   const name = req.file.originalname
+   const type = req.file.mimetype
+   const size = req.file.size
+   res.json({name, type, size})
+})
+
+const app_port = 3000
+app.listen(app_port, function () {
+   console.log(`This app is listening on port ${app_port}`)
+})
